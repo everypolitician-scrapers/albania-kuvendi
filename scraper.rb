52b58071864a592fa335e34db0b944b3b17f2f42
Tony Bowden <tony@tmtm.com>
2016-03-13 10:30:39 +0000

    Don't do approx matching here (do it at merge)

--- scraper.rb
+++ scraper.rb
@@ -4,25 +4,9 @@
 require 'scraperwiki'
 require 'nokogiri'
 require 'colorize'
-require 'fuzzy_match'
 require 'pry'
-require 'rest-client'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-
-def _prior_names
-  morph_api_key = ENV["MORPH_API_KEY"]
-  api_url = 'https://api.morph.io/tmtmtmtm/albania-assembly-wp/data.json'
-
-  return JSON.parse(RestClient.get(api_url, params: {
-    key: morph_api_key,
-    query: "SELECT name FROM data"
-  }), symbolize_names: true).map { |n| n[:name] }
-end
-
-def fuzzer
-  @fuzzer ||= FuzzyMatch.new(_prior_names)
-end
 
 class String
   def tidy
@@ -55,12 +39,12 @@ def scrape_person(url)
   noko = noko_for(url)
   box = noko.css('.list-center-component')
 
-# Members
   sort_name = box.at_css('h1').text.tidy
+  name = sort_name.split(/,\s*/, 2).reverse.join(" ")
 
   data = { 
     id: File.basename(url, '.*')[/(\d+)/, 1],
-    name: sort_name.split(/,\s*/, 2).reverse.join(" "),
+    name: name,
     sort_name: sort_name,
     image: box.css('.dl_image_comment_text img/@src').text,
     birth_date: date_from(box.xpath('//text()[contains(.,"Datëlindja")]').text),
@@ -69,16 +53,6 @@ def scrape_person(url)
     term: '8',
     source: url.to_s,
   }
-
-  # Consolidate the name with the previous scraper
-  match = fuzzer.find_with_score(data[:name])
-  confidence = match[1].to_f * 100
-  if confidence > 70
-    # warn "Renaming #{data[:name]} to #{match[0]} (%.1f%%)".green % confidence if confidence < 90
-    data[:name] = match[0]
-  else
-    # warn "No good match for #{data[:name]} (#{match})".yellow if confidence > 50
-  end
 
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
   ScraperWiki.save_sqlite([:id, :term], data)
