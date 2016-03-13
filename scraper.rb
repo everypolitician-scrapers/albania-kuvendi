c255ae938ff3a4d7001cacba7ceb4c2569e55cf1
Tony Bowden <tony@tmtm.com>
2016-03-13 11:31:15 +0000

    Rewrite scraper
    The official website has changed format

--- scraper.rb
+++ scraper.rb
@@ -26,36 +26,49 @@ end
 
 def scrape_list(url)
   noko = noko_for(url)
-  noko.css('.li_standart_list .item_title a/@href').each do |href|
-    scrape_person(href.text)
+  noko.css('.fusion-portfolio-post').each do |p|
+    party_id, party = p.css('.fusion-portfolio-content h4').text.tidy.split(':',2)
+    data = { 
+      id: p.attr('class')[/post-(\d+)/, 1],
+      name: p.css('h2').text.tidy,
+      party_id: party_id,
+      party: party,
+      email: p.css('a[href*="@"]/@href').text,
+      term: 8,
+      image: p.css('.fusion-image-wrapper img/@src').text,
+      source: p.css('h2 a/@href').text,
+    }
+    data.merge! scrape_person(data[:source])
+    ScraperWiki.save_sqlite([:id, :term], data)
   end
 
-  unless (next_page = noko.css('span.next_page a/@href')).empty?
+  unless (next_page = noko.css('a.next/@href')).empty?
     scrape_list next_page.text
   end
 end
 
 def scrape_person(url)
   noko = noko_for(url)
-  box = noko.css('.list-center-component')
+  box = noko.css('.post-content')
+  cells = box.xpath('.//td').map { |t| t.text }.map { |t| t.split("\n") }.flatten.map { |t| t.tidy }.compact.reject { |t| t.empty? }
+  unless cells.any?
+    warn "No data at #{url}"
+    return {}
+  end
 
-  sort_name = box.at_css('h1').text.tidy
-  name = sort_name.split(/,\s*/, 2).reverse.join(" ")
+  member_h = cells.find_index { |t| t.start_with? 'Zgjedhur' } or binding.pry
+  groups_h  = cells.find_index { |t| t.start_with? 'Grupi' } 
+
+  member = cells[member_h+1 .. (groups_h ? groups_h-1 : -1)]
+  groups  = groups_h ? cells[groups_h+1 .. -1] : []
 
   data = { 
-    id: File.basename(url, '.*')[/(\d+)/, 1],
-    name: name,
-    sort_name: sort_name,
-    image: box.css('.dl_image_comment_text img/@src').text,
-    birth_date: date_from(box.xpath('//text()[contains(.,"Datëlindja")]').text),
-    faction: box.xpath('.//h1[contains(.,"Grupi")]/following::text()[contains(translate(.,"GP","gp"),"grupi parlamentar")]').text.tidy.sub(/Grupi Parlamentar[ :i]* /i,'').sub('-së','').sub(/\.$/,''),
-    area: box.xpath('.//h1[contains(.,"Zgjedhur")]/following::text()[contains(.,"Qarku")]').text.sub(/.*Qarku:?/,'').tidy,
-    term: '8',
-    source: url.to_s,
-  }
-
-  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite([:id, :term], data)
+    birth_date: date_from(cells.find { |t| t.include?('Datëlindja') }),
+    birth_place: cells.find(->{":"}) { |t| t.include?("Vendlindja") }.split(':', 2).last.tidy.sub(/\.$/,''),
+    area: member.find(->{":"}) { |t| t.include? "Qarku" }.split(':', 2).last.tidy.sub(/\.$/,''),
+    # faction: groups.find(->{":"}) { |t| t.include? "Grupi parlamentar:" }.split(':', 2).last.tidy.sub(/\.$/,''),
+    commissions: groups.find(->{""}) { |t| t.include? "Komisioni" }.tidy,
+  } rescue binding.pry
 end
 
-scrape_list('http://www.parlament.al/web/Jeteshkrime_7033_1.php')
+scrape_list('http://www.parlament.al/atribut/deputet/')
