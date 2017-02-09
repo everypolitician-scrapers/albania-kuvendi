bb275cab2e3a2bd58a978d24d1b2eeca4287562d
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-09 17:22:17 +0000

    Hoist individual test setup
    Tidies the test code by hoisting the setup outside of the individual
    tests and naming the cassettes from the urls.

--- test/member_test.rb
+++ test/member_test.rb
@@ -2,64 +2,49 @@
 require_relative './test_helper'
 require_relative '../lib/member_page.rb'
 
-describe 'Member page without any data' do
-  around { |test| VCR.use_cassette('AlfredPeza', &test) }
+describe 'member data' do
+  around { |test| VCR.use_cassette(url.split('/').last, &test) }
+  subject { MemberPage.new(response: Scraped::Request.new(url: url).response) }
 
-  subject do
-    url = 'https://www.parlament.al/deputet/alfred-peza/'
-    MemberPage.new(response: Scraped::Request.new(url: url).response)
+  describe 'Member page without any data' do
+    let(:url) { 'https://www.parlament.al/deputet/alfred-peza/' }
+
+    it 'should have the expected data' do
+      subject.to_h.must_equal(birth_date:  '',
+                              birth_place: '',
+                              area:        '')
+    end
   end
 
-  it 'should have the expected data' do
-    subject.to_h.must_equal(birth_date:  '',
-                            birth_place: '',
-                            area:        '')
-  end
-end
+  describe 'Member page with complete data' do
+    let(:url) { 'https://www.parlament.al/deputet/genc-ruli/' }
 
-describe 'Member page with complete data' do
-  around { |test| VCR.use_cassette('RuliGenc', &test) }
-
-  subject do
-    url = 'https://www.parlament.al/deputet/genc-ruli/'
-    MemberPage.new(response: Scraped::Request.new(url: url).response)
+    it 'should have the expected data' do
+      subject.to_h.must_equal(area:        'Gjirokastër',
+                              birth_date:  '1958-04-11',
+                              birth_place: 'Tiranë')
+    end
   end
 
-  it 'should have the expected data' do
-    subject.to_h.must_equal(area:        'Gjirokastër',
-                            birth_date:  '1958-04-11',
-                            birth_place: 'Tiranë')
-  end
-end
+  describe 'Member page without area data' do
+    let(:url) { 'https://www.parlament.al/deputet/agron-duka-2/' }
 
-describe 'Member page without area data' do
-  around { |test| VCR.use_cassette('DukaAgron', &test) }
-
-  subject do
-    url = 'https://www.parlament.al/deputet/agron-duka-2/'
-    MemberPage.new(response: Scraped::Request.new(url: url).response)
+    it 'should have the expected data' do
+      subject.to_h.must_equal(area:        '',
+                              birth_date:  '1958-01-22',
+                              birth_place: 'Shijak')
+    end
   end
 
-  it 'should have the expected data' do
-    subject.to_h.must_equal(area:        '',
-                            birth_date:  '1958-01-22',
-                            birth_place: 'Shijak')
-  end
-end
+  # There is a case where the text "Vendlindja" (birth place) is displayed
+  # but no birth date is given
+  describe 'Member without birth place data but with "Vendlindja:" on page' do
+    let(:url) { 'https://www.parlament.al/deputet/andrea-marto/' }
 
-# There is a case where the text "Vendlindja" (birth place) is displayed
-# but no birth date is given
-describe 'Member without birth place data but with "Vendlindja:" on page' do
-  around { |test| VCR.use_cassette('MartoAndrea', &test) }
-
-  subject do
-    url = 'https://www.parlament.al/deputet/andrea-marto/'
-    MemberPage.new(response: Scraped::Request.new(url: url).response)
-  end
-
-  it 'should have the expected data' do
-    subject.to_h.must_equal(area:        '',
-                            birth_place: '',
-                            birth_date:  '1957-09-05')
+    it 'should have the expected data' do
+      subject.to_h.must_equal(area:        '',
+                              birth_place: '',
+                              birth_date:  '1957-09-05')
+    end
   end
 end
