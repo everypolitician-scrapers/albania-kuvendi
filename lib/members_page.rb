8f17706eb3601d3b81f86f29c7a549ab03904f84
Oliver Denman <oliver@mysociety.org>
2017-03-02 10:35:55 +0000

    Extract MembersPage

---
+++ lib/members_page.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'scraped'
+
+class MembersPage < Scraped::HTML
+  field :member_rows do
+    noko.css('.fusion-portfolio-post').map do |row|
+      fragment row => MemberRow
+    end
+  end
+
+  field :next_page do
+    noko.css('a.next/@href')
+  end
+end
